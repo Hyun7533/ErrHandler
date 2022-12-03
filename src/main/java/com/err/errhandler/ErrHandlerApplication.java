a43f5e8cc700c61fbeef633127f1b5991f1ccc56
KimTaeHyun <xogus7533@naver.com>
2022-12-03 21:37:12 +0900

    Initial commit

---
+++ src/main/java/com/err/errhandler/ErrHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.err.errhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/err/errhandler/ErrHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.err.errhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
